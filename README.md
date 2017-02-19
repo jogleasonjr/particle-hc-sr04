23051bf638b14fdf5c132eec94a4708119735d78
John Gleason <jogleasonjr@gmail.com>
2017-02-18 23:11:02 -0600

    initial working build

---
+++ README.md
@@ -0,0 +1,24 @@
+# particle-hc-sr04
+
+Firmware for reading ultrasonic measurements from an HC-SR04 on a particle device.
+
+The firmware assigns the readings to `inches` and `cm` variables.
+
+## To build/deploy/test
+
+1. Clone this repo
+
+2. `cd particle-hc-sr04` 
+
+3. `particle flash YOUR_DEVICE_NAME_OR_ID particle.ino`
+
+4. `particle monitor YOUR_DEVICE_NAME_OR_ID inches`
+
+## Note
+
+You'll need to power the HC-SR04 externally with 5V. Undervolting it by using the Particle's 3.3V pin will interrupt the Particle's WiFi while we wait for the HC's trigger pin.
+
+## Someday/maybe
+
+* Publish threshold events to the Particle cloud
+* Implement smart interrupts if the TRIG pin times out
--- particle.ino
+++ particle.ino
@@ -1,40 +1,22 @@
-/**
- * HC-SR04 Demo
- * Demonstration of the HC-SR04 Ultrasonic Sensor
- * Date: August 3, 2016
- * 
- * Description:
- *  Connect the ultrasonic sensor to the Arduino as per the
- *  hardware connections below. Run the sketch and open a serial
- *  monitor. The distance read from the sensor will be displayed
- *  in centimeters and inches.
- * 
- * Hardware Connections:
- *  Arduino | HC-SR04 
- *  -------------------
- *    5V    |   VCC     
- *    7     |   Trig     
- *    8     |   Echo     
- *    GND   |   GND
- *  
- * License:
- *  Public Domain
- */
+#define TRIG_PIN D6
+#define ECHO_PIN D4
 
-// Pins
-const int TRIG_PIN = 7;
-const int ECHO_PIN = 8;
+// store variable for Particle.publish here
+double lastReadingInches = -1;
+double lastReadingCm = -1;
 
 // Anything over 400 cm (23200 us pulse) is "out of range"
 const unsigned int MAX_DIST = 23200;
 
 void setup() {
 
+  Particle.variable("inches", lastReadingInches);
+  Particle.variable("centimeters", lastReadingCm);
+
   // The Trigger pin will tell the sensor to range find
   pinMode(TRIG_PIN, OUTPUT);
   digitalWrite(TRIG_PIN, LOW);
 
-  // We'll use the serial monitor to view the sensor output
   Serial.begin(9600);
 }
 
@@ -43,8 +25,6 @@ void loop() {
   unsigned long t1;
   unsigned long t2;
   unsigned long pulse_width;
-  float cm;
-  float inches;
 
   // Hold the trigger pin high for at least 10 us
   digitalWrite(TRIG_PIN, HIGH);
@@ -52,6 +32,7 @@ void loop() {
   digitalWrite(TRIG_PIN, LOW);
 
   // Wait for pulse on echo pin
+  // TODO: set a timeout so we don't lose connection to the particle cloud server
   while ( digitalRead(ECHO_PIN) == 0 );
 
   // Measure how long the echo pin was held high (pulse width)
@@ -63,17 +44,17 @@ void loop() {
 
   // Calculate distance in centimeters and inches. The constants
   // are found in the datasheet, and calculated from the assumed speed 
-  //of sound in air at sea level (~340 m/s).
-  cm = pulse_width / 58.0;
-  inches = pulse_width / 148.0;
+  // of sound in air at sea level (~340 m/s).
+  lastReadingCm = pulse_width / 58.0;
+  lastReadingInches = pulse_width / 148.0;
 
   // Print out results
   if ( pulse_width > MAX_DIST ) {
     Serial.println("Out of range");
   } else {
-    Serial.print(cm);
+    Serial.print(lastReadingCm);
     Serial.print(" cm \t");
-    Serial.print(inches);
+    Serial.print(lastReadingInches);
     Serial.println(" in");
   }
   
